7bafb4a99363a5ffac1623c134652e2da0254abb
L1nixhvh <dennis.tihomirov@gmail.com>
2024-04-11 21:17:20 +0300

    commit

--- database/db.py
+++ database/db.py
@@ -10,6 +10,7 @@ class dbConnection:
         load_dotenv()
 
     def get_engine(self):
+        
         url = os.getenv("URL")
         engine = create_engine(url, pool_size=50, echo=False)
         return engine
