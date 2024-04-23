d25ecf639581970ef96c8a7b05d8f1d2fada20cd
L1nixhvh <dennis.tihomirov@gmail.com>
2024-04-23 21:28:33 +0300

    added JWT on register

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from dotenv import load_dotenv
 import services
 from sqlalchemy.orm import sessionmaker
@@ -9,41 +9,49 @@ import datetime
 import os
 
 app = Flask(__name__)
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config["JWT_SECRET_KEY"] = SECRET_KEY
+app.config['SECRET_KEY'] = os.urandom(32)
+app.config["JWT_SECRET_KEY"] = os.urandom(32)
+app.config["JWT_TOKEN_LOCATION"] = ['headers']
 
 @app.route("/login", methods=["POST"])
 def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
 
-    username = request.form["username"]
-    password = request.form["password"]
-
-    user = user_table.FindUser(username)
-    try:
-        if user and hasher.verify(user.password, password):
-            # логика сессий
-            access_token = create_access_token(identity=user.id, expires_delta=datetime.timedelta(hours=2))
-            return jsonify({'message': 'Login Success', 'access_token': access_token})
-    except VerifyMismatchError:
-        return jsonify({"Operation": "Not successful"})
+    username = request.json.get("username")
+    password = request.json.get("password")
+    if not username or not password:
+        return jsonify({"msg": "Missing username or password"}), 400
     
-    return jsonify({"Operation": "Not successful"})
+    user = user_table.FindUser(username)
+    try:    
+        if user and hasher.verify(user.password, password):
+            access_token = create_access_token(identity=user.id, expires_delta=datetime.timedelta(hours=2))
+            return jsonify({'msg': 'Login success', 'access_token': access_token}), 200
+    except VerifyMismatchError:
+        return jsonify({"msg": "Bad Credentials"}), 401
+    
+    return jsonify({"msg": "Not successful"}), 400
     
     
 @app.route("/register", methods=["POST"])
 def register():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
 
-    username = request.form["username"]
-    password = request.form["password"]
+    username = request.json.get("username")
+    password = request.json.get("password")
+    if not username or not password:
+        return jsonify({"msg": "Missing username or password"}), 400
 
     user = user_table.FindUser(username)
-
-    if user is None:
-        user_table.InsertUser(username, user_table.EncryptedPassword(password))
-        # возвращаем сессию
-
-    return jsonify({"Operation": "login is already registered"})
+    if user is not None:
+        return jsonify({"msg": "Login is already registered"}), 401
+    if not user_table.InsertUser(username, user_table.EncryptedPassword(password)):
+        return jsonify({"msg": "Insert user error"}), 400
+    
+    access_token = create_access_token(identity=user.id, expires_delta=datetime.timedelta(hours=2))
+    return jsonify({'msg': 'User created', 'access_token': access_token}), 200
 
 
 if __name__ == "__main__":
