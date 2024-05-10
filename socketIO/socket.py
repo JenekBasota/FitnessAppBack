136e0d0d433cb45d111689dfc5d44d44cb5456ba
L1nixhvh <dennis.tihomirov@gmail.com>
2024-05-10 14:20:43 +0300

    added model

--- socketIO/socket.py
+++ socketIO/socket.py
@@ -1,18 +1,43 @@
 from flask_socketio import SocketIO, emit
+from model.Classes.utils import Utils
+from model.Classes.ClassificationSmoothing import EMADictSmoothing
+from model.Classes.FullBodyPoseEmbedder import FullBodyPoseEmbedder
+from model.Classes.PoseClassifier import PoseClassifier
 
+pose_samples_folder = "model/fitness_poses_images_csv"
+model_utils = Utils()
+
+def StartClassifier(data):
+    pose_embedder = FullBodyPoseEmbedder()
+    pose_classifier = PoseClassifier(
+        pose_samples_folder=pose_samples_folder,
+        pose_embedder=pose_embedder,
+        top_n_by_max_distance=30,
+        top_n_by_mean_distance=10,
+    )
+
+    transformedLandmarks = model_utils.read_landmark_from_json(data)
+
+    if transformedLandmarks is not None:
+        if transformedLandmarks.shape != (33,3): 
+            return f"Unexpected landmarks shape: {transformedLandmarks.shape}"
+
+        pose_classification = pose_classifier(transformedLandmarks)
+
+        classification_filter = EMADictSmoothing(window_size=10, alpha=0.2)
+
+        pose_classification_filtered = classification_filter(pose_classification)
+
+        return pose_classification_filtered
+    
 socketio = SocketIO()
 
 @socketio.on('message')
 def handle_message(data):
     try:
-        # Пытаемся разобрать JSON-данные
         if 'landmark' in data:
-            landmarks = data['landmark']
-
-            for landmark in landmarks:
-                print(f"Received landmark data: {landmark}")
-
-            emit('message', {'status': 'success', 'message': 'Data received'})
+            result = StartClassifier(data)
+            emit('message', {'status': 'success', 'msg': result})
         else:
             emit('message', {'status': 'error', 'message': 'JSON data is missing the landmark key'})
     except:
