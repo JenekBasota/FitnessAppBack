3634d974e418022db90a97e2754166f5be7c795e
L1nixhvh <dennis.tihomirov@gmail.com>
2024-05-13 13:54:23 +0300

    added json for test and update sockets

--- socketIO/socket.py
+++ socketIO/socket.py
@@ -1,11 +1,13 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from model.Classes.ClassificationSmoothing import EMADictSmoothing
 from model.Classes.FullBodyPoseEmbedder import FullBodyPoseEmbedder
 from model.Classes.PoseClassifier import PoseClassifier
+from datetime import datetime
 
 pose_samples_folder = "model/fitness_poses_images_csv"
 
 socketio = SocketIO()
+room_data = {}
 
 def StartClassifier(data):
     pose_embedder = FullBodyPoseEmbedder()
@@ -33,10 +35,65 @@ def StartClassifier(data):
 @socketio.on('message')
 def handle_message(data):
     try:
-        if 'landmark' in data:
-            result = StartClassifier(data)
-            emit('message', {'status': 'success', 'msg': result})
-        else:
+        if 'room_id' not in data or data['room_id'] not in room_data:
+            emit('message', {'status': 'error', 'msg': "Room identifier not found"})
+            return 0
+        
+        if 'exercise' not in data:
+            emit('message', {'status': 'error', 'msg': "Exercise not found"})
+            return 0
+
+        if 'landmark' not in data:
             emit('message', {'status': 'error', 'message': 'JSON data is missing the landmark key'})
+            return 0
+        
+        room_id = data['room_id']
+        room_data[room_id]['exercise'] = data['exercise']
+
+        result = StartClassifier(data)
+        current_phase_values = list(result.values())
+        current_phase_keys = list(result.keys())
+        if room_data[room_id]['current phase'] is None:
+            room_data[room_id]['current phase'] = current_phase_keys[0]
+        else:
+            if len(current_phase_values) == 1 or current_phase_values[0] >= current_phase_values[1]:
+                target_phase = current_phase_keys[0]
+            else:
+                target_phase = current_phase_keys[1]
+                
+            if room_data[room_id]['current phase'] != target_phase:
+                room_data[room_id]['current phase'] = target_phase
+                if "up" not in target_phase:
+                    room_data[room_id]['count'] += 1
+
+
+        emit('message', {'status': 'success', 'count': room_data[room_id]['count']}, room=room_id)
     except:
         emit('message', {'status': 'error', 'msg': 'Invalid message'})
+
+@socketio.on('connect')
+def create_room():
+    room_id = 'room_' + datetime.now().strftime('%Y%m%d%H%M%S%f')[:-3]
+    room_data[room_id] = {
+        'start': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'exercise' : None,
+        'count': 0,
+        'current phase': None,
+        'end': None}
+    join_room(room_id)
+    emit('message', {'room_id': room_id}, room=room_id)
+
+@socketio.on('exit_room')
+def exit_room(data):
+    if 'room_id' not in data:
+        emit('message', {'status': 'error', 'msg': "Room identifier not found"})
+        return 0
+    
+    room_id = data['room_id']
+    room_data[room_id]['end'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    emit('message', {'status': 'success', 'msg': "Leave room"}, room=room_id)
+    
+    leave_room(room_id)
+    del room_data[room_id]
+    
