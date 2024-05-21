8c2cf9ff25fed33deeb9d6ed8d2e04a2be86f06b
L1nixhvh <dennis.tihomirov@gmail.com>
2024-05-21 12:46:25 +0300

    fix CD

--- server.py
+++ server.py
@@ -12,27 +12,36 @@ app = Flask(__name__)
 cors = CORS()
 jwt = JWTManager()
 
+
 def setup_app_context():
     with app.app_context():
-        current_app.session_bd = sessionmaker(bind=dbConnectionEngine().get_engine().connect())()
+        current_app.session_bd = sessionmaker(
+            bind=dbConnectionEngine().get_engine().connect()
+        )()
 
-@app.route('/docs/swagger.json')
+
+@app.route("/docs/swagger.json")
 def send_docs():
-    return send_from_directory(os.path.join(app.root_path, 'routes', 'swagger', 'docs'), 'swagger.json')
+    return send_from_directory(
+        os.path.join(app.root_path, "routes", "swagger", "docs"), "swagger.json"
+    )
+
 
 if __name__ == "__main__":
     load_dotenv()
-    app.config['SECRET_KEY'] = os.urandom(64)
+    app.config["SECRET_KEY"] = os.urandom(64)
     app.config["JWT_SECRET_KEY"] = os.urandom(64)
-    app.config["JWT_TOKEN_LOCATION"] = ['headers']
+    app.config["JWT_TOKEN_LOCATION"] = ["headers"]
     setup_app_context()
 
-    app.register_blueprint(auth_Blueprint, url_prefix='/api/auth')
-    app.register_blueprint(jwt_Blueprint, url_prefix='/api/jwt')
+    app.register_blueprint(auth_Blueprint, url_prefix="/api/auth")
+    app.register_blueprint(jwt_Blueprint, url_prefix="/api/jwt")
     app.register_blueprint(swaggerui_blueprint)
-    
+
     jwt.init_app(app)
     socketio.init_app(app)
     cors.init_app(app, resources={r"/*": {"origins": "*"}})
-    
-    socketio.run(app, debug=False, host="0.0.0.0", port=443, allow_unsafe_werkzeug=True)
+
+    socketio.run(
+        app, debug=False, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True
+    )
